f8ad0d7a26dfbbb568d917383540b63e6928fd58
Aliaksandr Kaptsevich <carcer2007@gmail.com>
2024-05-18 22:20:54 +0200

    add: getting the name from the local storage all exercises, their display and the ability to delete an exercise.

--- common/component/ExerciseList/ExerciseList.tsx
+++ common/component/ExerciseList/ExerciseList.tsx
@@ -1,37 +1,58 @@
-import React from 'react';
-import {View, Text, TouchableOpacity, FlatList} from 'react-native';
-import {Exercise} from "../HomeScreen/HomeScreen";
+import React, {useEffect, useState} from 'react';
+import {View, Text, TouchableOpacity, FlatList, Alert, Button} from 'react-native';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
 type ExerciseListProps = {
-    exercises: Exercise[];
-    onSelectExercise: (exercise: Exercise) => void;
+    // onSelectExercise: (exercise: string) => void;
+    //TODO fix this type
     navigation: any;
 }
 
 export const ExerciseList = (props: ExerciseListProps) => {
 
-    const handleSelectExercise = (selectedExercise: Exercise) => {
+    const [exerciseKeys, setExerciseKeys] = useState<readonly string[]>([]);
+    const fetchData = async () => {
+        const keys = await AsyncStorage.getAllKeys();
+        setExerciseKeys(keys);
+    };
+    useEffect(() => {
+         fetchData();
+    }, [exerciseKeys]);
+
+    const handleSelectExercise = (selectedExercise: string) => {
         props.navigation.navigate('Exercise', {exercise: selectedExercise});
     };
+
     return (
         <View>
             <Text style={{width: 200, height: 50, fontSize: 18, textAlign: "center"}}>Выберите упражнение:</Text>
 
             <FlatList
-                data={props.exercises}
+                data={exerciseKeys}
                 renderItem={({item}) => (
-                    <TouchableOpacity onPress={() => handleSelectExercise(item)}>
+                    <TouchableOpacity onPress={() => handleSelectExercise(item)}
+                                      style={{ flexDirection: 'row' }}
+                    >
                         <Text style={{
                             width: 200,
                             height: 50,
                             fontSize: 18,
                             textAlign: "center"
-                        }}>{item.exerciseName}</Text>
+                        }}>{item}</Text>
+                        <Button title={"Del"} 
+                                onPress={ async ()=>{
+                            try {
+                                await AsyncStorage.removeItem(`${item}`)
+                                await fetchData()
+                            } catch(e) {
+                                Alert.alert('Ошибка', `${e}`);
+                            }
+                        }}></Button>
                     </TouchableOpacity>
 
                 )}
-                keyExtractor={(item) => item.id.toString()}
+                keyExtractor={(item) => item}
             />
         </View>
     );
--- common/component/HomeScreen/HomeScreen.tsx
+++ common/component/HomeScreen/HomeScreen.tsx
@@ -2,20 +2,21 @@ import {useState} from "react";
 import {useNavigation} from "@react-navigation/native";
 import {AddExerciseForm, ExerciseData} from "../AddExerciseForm/AddExerciseForm";
 import uuid from "react-native-uuid";
-import {Alert, View} from "react-native";
+import {Alert, Button, View} from "react-native";
 import {ExerciseList} from "../ExerciseList/ExerciseList";
 import * as React from "react";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export type Exercise = {
-    exerciseName: string
-    id: string | number[]
-}
+// export type Exercise = {
+//     exerciseName: string
+//     id: string | number[]
+// }
+
 
 
 
 export function HomeScreen() {
-    const [exercises, setExercises] = useState<Exercise[]>([]);
+    // const [exercises, setExercises] = useState<Exercise[]>([]);
     const navigation = useNavigation();
 
     const handleAddExercise = async (exerciseName: string) => {
@@ -26,34 +27,49 @@ export function HomeScreen() {
         // } else {
         //     Alert.alert(JSON.stringify("Напиши покороче. Тут ограничение в 35 символов"))
         // }
-        if (!exerciseName.trim()){
-            return  Alert.alert('Ошибка', 'Невозможно сохранить упражнение без названия');
+        if (!exerciseName.trim()) {
+            return Alert.alert('Ошибка', 'Невозможно сохранить упражнение без названия');
         }
-            try {
-            const key = `exercise_${exerciseName.trim()}`; // задаем ключ из названия категории и имени
-            await AsyncStorage.setItem(key, exerciseName);
+        try {
+            const defaultSettingsForExercise = {
+                exerciseName,
+                id:uuid.v4(),
+                exerciseStatic: {
+                    totalSets_local:null,
+                    totalWeight_local: null,
+                    maxWeight_local: null
+                }
+            }
+            const key = `${exerciseName.trim()}`; // задаем ключ из названия категории и имени
+            await AsyncStorage.setItem(key, JSON.stringify(defaultSettingsForExercise));
             Alert.alert('Сохранено!', `Упражнение "${exerciseName}" сохранено с ключом "${key}".`);
         } catch (e) {
             console.error('Ошибка сохранения упражнения', e);
             Alert.alert('Ошибка', 'Не удалось сохранить упражнение.');
         }
-        finally {
-                const keys = await AsyncStorage.getAllKeys()
-                const value = await AsyncStorage.getItem(`exercise_${exerciseName.trim()}`);
-                console.log(keys)
-            }
     };
-    const handleSelectExercise = (selectedExercise: Exercise) => {
-        Alert.alert(JSON.stringify(`Выбрано упражнение: ${selectedExercise.exerciseName}`))
-    };
+    // =============== А это НУЖНО?!
+    // const handleSelectExercise = (selectedExercise: string) => {
+    //     Alert.alert(JSON.stringify(`Выбрано упражнение: ${selectedExercise}`))
+    // };
     return (
         <>
             <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
                 <AddExerciseForm onAddExercise={handleAddExercise}/>
             </View>
             <View style={{flex: 2, alignItems: 'center', justifyContent: 'center'}}>
-                <ExerciseList exercises={exercises} onSelectExercise={handleSelectExercise} navigation={navigation}  />
+                <ExerciseList
+                    // exercises={exercises}
+                    // onSelectExercise={handleSelectExercise}
+                    navigation={navigation}/>
             </View>
+            <Button title={"Clean"} onPress={ async ()=>{
+                try {
+                    await AsyncStorage.clear()
+                } catch(e) {
+                    Alert.alert('Ошибка', `${e}`);
+                }
+            }}></Button>
         </>
 
 
