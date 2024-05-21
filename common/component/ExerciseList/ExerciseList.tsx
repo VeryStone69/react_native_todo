5395328006be89c736e71f91621ae7a9c51ec284
Aliaksandr Kaptsevich <carcer2007@gmail.com>
2024-05-21 21:09:28 +0200

    add: receiving data (exercises) via props, changing the name for correct display. update: exercise delete function, useEffect fires when new data is received

--- common/component/ExerciseList/ExerciseList.tsx
+++ common/component/ExerciseList/ExerciseList.tsx
@@ -4,53 +4,49 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
 type ExerciseListProps = {
-    // onSelectExercise: (exercise: string) => void;
+    allExerciseList: readonly string[]
     //TODO fix this type
     navigation: any;
 }
 
 export const ExerciseList = (props: ExerciseListProps) => {
+    const [exerciseData, setExerciseData] = useState<readonly string[]>([]);
 
-    const [exerciseKeys, setExerciseKeys] = useState<readonly string[]>([]);
-    const fetchData = async () => {
-        const keys = await AsyncStorage.getAllKeys();
-        setExerciseKeys(keys);
-    };
     useEffect(() => {
-         fetchData();
-    }, [exerciseKeys]);
+        setExerciseData(props.allExerciseList)
+
+    }, [props.allExerciseList]);
 
     const handleSelectExercise = (selectedExercise: string) => {
-        props.navigation.navigate('Exercise', {exercise: selectedExercise});
+        const exerciseName = selectedExercise.replace('exercise_list_', '');
+        props.navigation.navigate('Exercise', {exerciseName});
     };
+
+    const removeExercise = async (item: string) => {
+        try {
+            await AsyncStorage.removeItem(item);
+            const keys = await AsyncStorage.getAllKeys();
+            setExerciseData(keys.filter(key => key.startsWith('exercise_list_')));
+        } catch (e) {
+            Alert.alert('Ошибка', `${e}`);
+        }
+    };
+
+    //TODO: delete this console later
+    console.log("render_ExerciseList")
 
     return (
         <View>
             <Text style={{width: 200, height: 50, fontSize: 18, textAlign: "center"}}>Выберите упражнение:</Text>
-
             <FlatList
-                data={exerciseKeys}
+                data={exerciseData}
                 renderItem={({item}) => (
-                    <TouchableOpacity onPress={() => handleSelectExercise(item)}
-                                      style={{ flexDirection: 'row' }}
-                    >
-                        <Text style={{
-                            width: 200,
-                            height: 50,
-                            fontSize: 18,
-                            textAlign: "center"
-                        }}>{item}</Text>
-                        <Button title={"Del"} 
-                                onPress={ async ()=>{
-                            try {
-                                await AsyncStorage.removeItem(`${item}`)
-                                await fetchData()
-                            } catch(e) {
-                                Alert.alert('Ошибка', `${e}`);
-                            }
-                        }}></Button>
+                    <TouchableOpacity onPress={() => handleSelectExercise(item)} style={{flexDirection: 'row'}}>
+                        <Text style={{width: 200, height: 50, fontSize: 18, textAlign: 'center'}}>
+                            {item.replace('exercise_list_', '')}
+                        </Text>
+                        <Button title="Del" onPress={() => removeExercise(item)}/>
                     </TouchableOpacity>
-
                 )}
                 keyExtractor={(item) => item}
             />
