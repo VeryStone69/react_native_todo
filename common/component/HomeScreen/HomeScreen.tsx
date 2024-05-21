20c851a0bcb430e8e5625ed4f703b7fdb50756b2
Aliaksandr Kaptsevich <carcer2007@gmail.com>
2024-05-21 20:58:53 +0200

    add: useEffect to get a list of exercises, passing exercise names to the list.  update: to add exercise function

--- common/component/HomeScreen/HomeScreen.tsx
+++ common/component/HomeScreen/HomeScreen.tsx
@@ -1,34 +1,41 @@
 import {useNavigation} from "@react-navigation/native";
 import {AddExerciseForm} from "../AddExerciseForm/AddExerciseForm";
-import uuid from "react-native-uuid";
 import {Alert, View} from "react-native";
 import {ExerciseList} from "../ExerciseList/ExerciseList";
 import * as React from "react";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useEffect, useState} from "react";
 
 export function HomeScreen() {
+    const [exerciseKeys, setExerciseKeys] = useState<readonly string[]>([]);
     const navigation = useNavigation();
+
+    useEffect(() => {
+        const fetchExerciseKeys = async () => {
+            const keys = await AsyncStorage.getAllKeys();
+            setExerciseKeys(keys.filter(key => key.startsWith('exercise_list_')));
+        };
+
+        fetchExerciseKeys();
+    }, []);
 
     const handleAddExercise = async (exerciseName: string) => {
         if (!exerciseName.trim()) {
             return Alert.alert('Ошибка', 'Невозможно сохранить упражнение без названия');
         }
         try {
-            const defaultSettingsForExercise = {
-                exerciseName,
-                id: uuid.v4(),
-                exerciseStatic: {
-                    totalSets_local: null,
-                    totalWeight_local: null,
-                    maxWeight_local: null
-                }
-            }
-            const key = `${exerciseName.trim()}`; // задаем ключ из названия категории и имени
-            await AsyncStorage.setItem(key, JSON.stringify(defaultSettingsForExercise));
+            const key = `exercise_list_${exerciseName.trim()}`; // задаем ключ из названия категории и имени
+            await AsyncStorage.setItem(key, exerciseName);
+            const keys = await AsyncStorage.getAllKeys();
+            // setExerciseKeys(getAllkeys);
+            setExerciseKeys(keys.filter(key => key.startsWith('exercise_list_')));
         } catch (e) {
             Alert.alert('Ошибка', 'Не удалось сохранить упражнение.');
         }
     };
+
+    //TODO delete this console later
+    console.log("render_HomeScreen")
 
     return (
         <>
@@ -36,7 +43,7 @@ export function HomeScreen() {
                 <AddExerciseForm onAddExercise={handleAddExercise}/>
             </View>
             <View style={{flex: 2, alignItems: 'center', justifyContent: 'center'}}>
-                <ExerciseList navigation={navigation}/>
+                <ExerciseList navigation={navigation} allExerciseList={exerciseKeys}/>
             </View>
         </>
     );
